15ac9e41a37549560e5fb1db66d7b8901218601a
Serges YAMAKO <yamakoserges@gmail.com>
2024-03-11 19:08:11 +0100

    Full Stack Service Booking System Project with Spring Boot, Angular, and MySQL | [2024]

---
+++ src/main/java/com/gsoftcode/servicebankingsystem/ServiceBankingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.gsoftcode.servicebankingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBankingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceBankingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gsoftcode/servicebankingsystem/ServiceBankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsoftcode.servicebankingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBankingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
