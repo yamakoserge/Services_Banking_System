62d7d7634322f7af1c0deca366dff4641d0fc029
Serges YAMAKO <yamakoserges@gmail.com>
2024-03-16 16:32:50 +0100

    adding signupCompany and SimpleCorsFilter

---
+++ src/main/java/com/gsoftcode/servicebankingsystem/config/SimpleCorsFilter.java
@@ -0,0 +1,39 @@
+package com.gsoftcode.servicebankingsystem.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.Value;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class SimpleCorsFilter implements Filter {
+
+       public SimpleCorsFilter(){
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) res;
+        Map<String, String> map = new HashMap<>();
+        String originHeader = request.getHeader("origin");
+        response.setHeader("Access-control-Allow-Origin", originHeader);
+        response.setHeader("Access-control-Allow-Methods","POST, GET, PUT, OPTION, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Header","*");
+
+        if ("OPTION".equalsIgnoreCase(request.getMethod())){
+            response.setStatus( HttpServletResponse.SC_OK);
+        }else {
+            chain.doFilter(req, res);
+        }
+    }
+}
