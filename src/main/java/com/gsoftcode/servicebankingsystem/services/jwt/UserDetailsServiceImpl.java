761a723fadfbb5347d61cc9f09c73278585fa2d4
Serges YAMAKO <yamakoserges@gmail.com>
2024-03-18 23:22:20 +0100

    adding spring security, creating jwt utils & web security config in spring boot

---
+++ src/main/java/com/gsoftcode/servicebankingsystem/services/jwt/UserDetailsServiceImpl.java
@@ -0,0 +1,10 @@
+package com.gsoftcode.servicebankingsystem.services.jwt;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailService{
+
+
+
+}
