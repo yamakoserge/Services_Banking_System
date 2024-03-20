6204aaf0bdc2b4664a612e65f7e475621766ce93
Serges YAMAKO <yamakoserges@gmail.com>
2024-03-20 13:02:43 +0100

    Adding Jwt and PostMapping(authenticate)complet

--- src/main/java/com/gsoftcode/servicebankingsystem/controller/AuthenticationController.java
+++ src/main/java/com/gsoftcode/servicebankingsystem/controller/AuthenticationController.java
@@ -1,20 +1,47 @@
 package com.gsoftcode.servicebankingsystem.controller;
 
+import com.gsoftcode.servicebankingsystem.dto.AuthenticationRequest;
 import com.gsoftcode.servicebankingsystem.dto.SignupRequestDTO;
 import com.gsoftcode.servicebankingsystem.dto.UserDto;
+import com.gsoftcode.servicebankingsystem.entity.User;
+import com.gsoftcode.servicebankingsystem.repository.UserRepository;
 import com.gsoftcode.servicebankingsystem.services.authentication.AuthService;
+import com.gsoftcode.servicebankingsystem.services.jwt.UserDetailsServiceImpl;
+import com.gsoftcode.servicebankingsystem.utils.JwtUtil;
+import jakarta.servlet.http.HttpServletResponse;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 public class AuthenticationController {
 
     @Autowired
     private AuthService authService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public static final String TOKEN_PREFIX = "Bearer ";
+
+    public static String HEADER_STRING = "Authorization";
 
     @PostMapping("/client/sign-up")
     public ResponseEntity<?> singupClient(@RequestBody SignupRequestDTO signupRequestDTO){
@@ -29,14 +56,41 @@ public class AuthenticationController {
     }
 
     @PostMapping("/company/sign-up")
-    public ResponseEntity<?> singupCompany(@RequestBody SignupRequestDTO signupRequestDTO){
+    public ResponseEntity<?> signupCompany(@RequestBody SignupRequestDTO signupRequestDTO){
         if (authService.presentByEmail(signupRequestDTO.getEmail())){
             return  new ResponseEntity<>("Cette Company existe déjà!", HttpStatus.NOT_ACCEPTABLE);
         }
-        UserDto createUser = authService.signupClient(signupRequestDTO);
+        UserDto createUser= authService.signupCompany(signupRequestDTO);
+
 
         return new ResponseEntity<>(createUser,HttpStatus.OK);
 
+    }
+
+    @PostMapping({"/authenticate"})
+    public void  createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest, HttpServletResponse response) throws IOException, JSONException {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                    authenticationRequest.getUsername(),authenticationRequest.getPassword()));
+        }catch (BadCredentialsException e){
+            throw new BadCredentialsException("Incorrect username or password", e);
+        }
+
+        final UserDetails userDetails= userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+        final  String jwt = jwtUtil.generateToken(userDetails.getUsername());
+        User user = userRepository.findFirstByEmail(authenticationRequest.getUsername());
+
+        response.getWriter().write(new JSONObject()
+                .put("userId", user.getId())
+                .put("role", user.getRole())
+                .toString()
+        );
+        response.addHeader("Access-Control-Expose-Headers", "Authorization");
+        response.addHeader("Access-Control-Allow-Headers", "Authorization" +
+                "X-PINGOTHER, origin, X-Request-with, Content-Type, X-Custom-header");
+
+        response.addHeader(HEADER_STRING, TOKEN_PREFIX+ jwt);
 
     }
 }
---
+++ src/main/java/com/gsoftcode/servicebankingsystem/dto/AuthenticationRequest.java
@@ -0,0 +1,11 @@
+package com.gsoftcode.servicebankingsystem.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthenticationRequest {
+
+        private String username;
+
+        private String password;
+    }
